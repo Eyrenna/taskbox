fede0bcd21d17b34e5ac501c4238f6b4c87f3841
Claudia Gázquez Alessio <cgazquez@intricom.es>
2021-09-07 10:21:06 +0200

    Task & Task.stories js

---
+++ src/components/Task.stories.js
@@ -0,0 +1,36 @@
+import React from 'react';
+
+import Task from './Task';
+
+export default {
+  component: Task,
+  title: 'Task',
+};
+
+const Template = args => <Task {...args} />;
+
+export const Default = Template.bind({}); //.bind copies a function
+Default.args = {                //allow us to live edit our components with the controls addon without restarting Storybook.
+  task: {                       //shape of component Task
+    id: '1',
+    title: 'Test Task',
+    state: 'TASK_INBOX',
+    updatedAt: new Date(2021, 0, 1, 9, 0),
+  },
+};
+
+export const Pinned = Template.bind({});
+Pinned.args = {
+  task: {
+    ...Default.args.task,
+    state: 'TASK_PINNED',
+  },
+};
+
+export const Archived = Template.bind({});
+Archived.args = {
+  task: {
+    ...Default.args.task,
+    state: 'TASK_ARCHIVED',
+  },
+};
