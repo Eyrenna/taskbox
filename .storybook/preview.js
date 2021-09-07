aec2193d8bbebfc9d4252d9dd45115bdcb075b1a
Claudia Gázquez Alessio <cgazquez@intricom.es>
2021-09-07 11:30:02 +0200

    config stories & CSS file

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,5 +1,7 @@
-export const parameters = {
-  actions: { argTypesRegex: "^on[A-Z].*" },
+import '../src/index.css';
+export const parameters = {   //'parameters' used to control the behaviour of Storybook's features and addons
+                              //'actions' allows us to create callbacks that appear in the actions panel of the Storybook UI when clicked
+  actions: { argTypesRegex: "^on[A-Z].*" }, //Configures Storybook to log the actions( onArchiveTask and onPinTask ) in the UI.
   controls: {
     matchers: {
       color: /(background|color)$/i,
