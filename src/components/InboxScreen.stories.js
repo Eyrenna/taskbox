b3692c8a2c29a7ab327167f9d1b5ef7e9d5242c9
Claudia Gázquez Alessio <cgazquez@intricom.es>
2021-09-08 09:49:10 +0200

    supplying context with decorators

--- src/components/InboxScreen.stories.js
+++ src/components/InboxScreen.stories.js
@@ -1,8 +1,33 @@
 import React from 'react';
-import { storiesOf } from '@storybook/react';
+import { Provider } from 'react-redux';
 
 import { PureInboxScreen } from './InboxScreen';
+import { action } from '@storybook/addon-actions';
 
-storiesOf('InboxScreen', module)
-  .add('default', () => <PureInboxScreen />)
-  .add('error', () => <PureInboxScreen error="Something" />);
+import * as TaskListStories from './TaskList.stories';
+
+// A super-simple mock of a redux store
+const store = {
+  getState: () => {
+   return {
+     tasks: TaskListStories.Default.args.tasks,
+   };
+  },
+  subscribe: () => 0,
+  dispatch: action('dispatch'),
+};
+
+export default {
+  component: PureInboxScreen,
+  decorators: [story => <Provider store={store}>{story()}</Provider>],
+  title: 'InboxScreen',
+};
+
+const Template = args => <PureInboxScreen {...args} />;
+
+export const Default = Template.bind({});
+
+export const Error = Template.bind({});
+Error.args = {
+  error: 'Something',
+};
